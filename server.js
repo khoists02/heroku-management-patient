4608c4eff58bbe7a5ffec75ceaa1b2377727a48b
khoi.minh <khoi.kioto@gmail.com>
2020-11-05 13:12:00 +0700

    updated

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080;
 
-server.use(middlewares)
-server.use(router)
-server.listen(8080, () => {
-  console.log('JSON Server is running')
-})
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
